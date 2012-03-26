9ec74d0b7500647fd39173d9f36e919d6c6c9eb2
jonglee <jonglee1@illinois.edu>
2012-03-26 15:33:07 -0500

    added findByUri method

--- cyberintegrator-springdata/src/main/java/edu/illinois/ncsa/cyberintegrator/springdata/WorkflowDAO.java
+++ cyberintegrator-springdata/src/main/java/edu/illinois/ncsa/cyberintegrator/springdata/WorkflowDAO.java
@@ -36,4 +36,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import edu.illinois.ncsa.cyberintegrator.domain.Workflow;
 
 public interface WorkflowDAO extends PagingAndSortingRepository<Workflow, String> {
+    Workflow findByUri(String uri);
 }
