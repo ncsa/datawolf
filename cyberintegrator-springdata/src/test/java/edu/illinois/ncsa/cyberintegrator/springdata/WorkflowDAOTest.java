64bad49c251d58b01c5feb0e0a456f993a7acb90
Rob Kooper <kooper@illinois.edu>
2012-04-07 00:52:54 -0500

    use testContext.xml to prevent collision. clean up pom to depend on spring-libraries.

--- cyberintegrator-springdata/src/test/java/edu/illinois/ncsa/cyberintegrator/springdata/WorkflowDAOTest.java
+++ cyberintegrator-springdata/src/test/java/edu/illinois/ncsa/cyberintegrator/springdata/WorkflowDAOTest.java
@@ -15,7 +15,7 @@ public class WorkflowDAOTest {
 
     @BeforeClass
     public static void setUp() throws Exception {
-        SpringData.loadXMLContext("applicationContext.xml");
+        SpringData.loadXMLContext("testContext.xml");
     }
 
     @Test
