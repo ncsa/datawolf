5e02acba6b8fa8c8d85b6948d94cb9bb18bdcdfa
jonglee <jonglee1@illinois.edu>
2012-04-09 14:53:49 -0500

    removed findByUirString since we don't need URI anymore

--- cyberintegrator-springdata/src/main/java/edu/illinois/ncsa/cyberintegrator/springdata/WorkflowDAO.java
+++ cyberintegrator-springdata/src/main/java/edu/illinois/ncsa/cyberintegrator/springdata/WorkflowDAO.java
@@ -36,6 +36,4 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import edu.illinois.ncsa.cyberintegrator.domain.Workflow;
 
 public interface WorkflowDAO extends PagingAndSortingRepository<Workflow, String> {
-    Workflow findByUriString(String uri);
-
 }
