9d288028c80da7d25fd981350835f49a5c2c81f1
Rob Kooper <kooper@illinois.edu>
2012-04-09 21:20:24 -0500

    Add filestorage helper.

---
+++ ncsa-common-springdata/src/main/java/edu/illinois/ncsa/springdata/FileStorage.java
@@ -0,0 +1,162 @@
+/**
+ * 
+ */
+package edu.illinois.ncsa.springdata;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.illinois.ncsa.domain.FileDescriptor;
+
+/**
+ * @author Rob Kooper <kooper@illinois.edu>
+ * 
+ */
+public class FileStorage {
+    private static Logger logger = LoggerFactory.getLogger(FileStorage.class);
+
+    private int           levels;
+    private File          folder;
+
+    public FileStorage() {
+        setLevels(2);
+        setFolder(new File(System.getProperty("java.io.tmpdir")));
+    }
+
+    /**
+     * @return the levels
+     */
+    public int getLevels() {
+        return levels;
+    }
+
+    /**
+     * @param levels
+     *            the levels to set
+     */
+    public void setLevels(int levels) {
+        this.levels = levels;
+    }
+
+    /**
+     * @return the folder
+     */
+    public File getFolder() {
+        return folder;
+    }
+
+    /**
+     * @param folder
+     *            the folder to set
+     */
+    public void setFolder(File folder) {
+        this.folder = folder;
+    }
+
+    public FileDescriptor storeFile(InputStream is) throws IOException {
+        return storeFile(new FileDescriptor().getId(), null, is);
+    }
+
+    public FileDescriptor storeFile(String filename, InputStream is) throws IOException {
+        return storeFile(new FileDescriptor().getId(), filename, is);
+    }
+
+    public URL storeFile(FileDescriptor fd, InputStream is) throws IOException {
+        FileDescriptor check = storeFile(fd.getId(), fd.getFilename(), is);
+
+        // simple checks
+        if ((fd.getMd5sum() != null) && !fd.getMd5sum().equals(check.getMd5sum())) {
+            throw (new IOException("MD5SUM does not match, error saving data."));
+        }
+        if ((fd.getSize() > 0) && (fd.getSize() != check.getSize())) {
+            throw (new IOException("File size does not match, error saving data."));
+        }
+
+        // set the url to the new url
+        fd.setDataURL(check.getDataURL());
+
+        return check.getDataURL();
+    }
+
+    private FileDescriptor storeFile(String id, String filename, InputStream is) throws IOException {
+        FileDescriptor fd = new FileDescriptor();
+        fd.setId(id);
+        fd.setFilename(filename);
+
+        String path = "";
+
+        // create the folders
+        for (int i = 0; i < levels * 2 && id.length() >= i + 2; i += 2) {
+            path = path + id.substring(i, i + 2) + File.separator;
+        }
+        if (id.length() > 0) {
+            path = path + id + File.separator;
+        }
+
+        // append the filename
+        if (filename != null) {
+            path = path + filename;
+        } else {
+            path = path + "unknown.unk";
+        }
+
+        // create the output file
+        File output = new File(folder, path);
+        if (!output.getParentFile().isDirectory()) {
+            if (!output.getParentFile().mkdirs()) {
+                throw (new IOException("Could not create folder to store data in."));
+            }
+        }
+
+        // Write the file to disk, storing the md5sum
+        FileOutputStream fos = null;
+        MessageDigest md5 = null;
+        int size = 0;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            logger.error("Could not create MD5 digest.", e);
+        }
+        try {
+            fos = new FileOutputStream(output);
+            byte[] buf = new byte[10240];
+            int len = 0;
+            while ((len = is.read(buf)) >= 0) {
+                fos.write(buf, 0, len);
+                if (md5 != null) {
+                    md5.update(buf, 0, len);
+                }
+                size += len;
+            }
+        } finally {
+            if (fos != null) {
+                fos.close();
+            }
+            is.close();
+        }
+
+        // create the fd fields
+        // TODO RK : add mimetype
+        fd.setDataURL(output.toURI().toURL());
+        if (md5 != null) {
+            fd.setMd5sum(new BigInteger(md5.digest()));
+        }
+        fd.setSize(size);
+
+        return fd;
+    }
+
+    public InputStream readFile(FileDescriptor fd) throws IOException {
+        // TODO RK : add some caching
+        return fd.getDataURL().openStream();
+    }
+}
---
+++ ncsa-common-springdata/src/test/java/edu/illinois/ncsa/springdata/FileStorageTest.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package edu.illinois.ncsa.springdata;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+import org.junit.Test;
+
+import edu.illinois.ncsa.domain.FileDescriptor;
+
+/**
+ * @author Rob Kooper <kooper@illinois.edu>
+ * 
+ */
+public class FileStorageTest {
+
+    @Test
+    public void testStore() throws Exception {
+        String x = "Some text";
+
+        FileDescriptor fd = new FileDescriptor();
+        fd.setFilename("text.txt");
+        fd.setId("11223344556677");
+        fd.setMd5sum(new BigInteger(MessageDigest.getInstance("MD5").digest(x.getBytes())));
+        fd.setSize(x.length());
+
+        FileStorage fs = new FileStorage();
+        fs.setLevels(3);
+
+        fs.storeFile(fd, new ByteArrayInputStream(x.getBytes()));
+
+        assertTrue(fd.getDataURL().toExternalForm().endsWith("/11/22/33/11223344556677/text.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(fs.readFile(fd)));
+        String line = br.readLine();
+        assertEquals(x, line);
+
+        line = br.readLine();
+        assertEquals(null, line);
+
+        br.close();
+    }
+}
