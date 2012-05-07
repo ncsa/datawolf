cadd25d971751c52aeb423b32bd57145b192bd1e
jonglee <jonglee1@illinois.edu>
2012-05-07 12:13:48 -0500

    fixed post test

---
+++ file-service/src/test/java/edu/illinois/ncsa/file/service/FileServicePostTest.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package edu.illinois.ncsa.file.service;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtil;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.illinois.ncsa.domain.FileDescriptor;
+import edu.illinois.ncsa.springdata.FileStorage;
+import edu.illinois.ncsa.springdata.SpringData;
+
+/**
+ * @author Rob Kooper <kooper@illinois.edu>
+ * 
+ */
+public class FileServicePostTest {
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        RestServer.jettyServer("src/test/resources", "testContext.xml");
+    }
+
+    @Test
+    public void testPost() throws Exception {
+        FileServiceClient.SERVER = "http://localhost:8088";
+
+        File postfile = new File("src/test/resources/test.txt");
+        InputStream o = new FileInputStream(postfile);
+        byte[] oba = IOUtil.toByteArray(o);
+        FileDescriptor fd = FileServiceClient.post(postfile);
+
+        FileStorage fs = SpringData.getFileStorage();
+        InputStream r = fs.readFile(fd);
+        byte[] ba = IOUtil.toByteArray(r);
+
+        assertArrayEquals(oba, ba);
+    }
+
+}
