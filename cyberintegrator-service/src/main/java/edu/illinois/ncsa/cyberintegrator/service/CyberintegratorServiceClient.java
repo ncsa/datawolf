4a8e323263f35462c4bdb9e1b0b6bba023837ee7
Jong Lee <jonglee1@illinois.edu>
2012-09-04 16:13:57 -0500

    updated CI-service client

--- cyberintegrator-service/src/main/java/edu/illinois/ncsa/cyberintegrator/service/CyberintegratorServiceClient.java
+++ cyberintegrator-service/src/main/java/edu/illinois/ncsa/cyberintegrator/service/CyberintegratorServiceClient.java
@@ -3,7 +3,10 @@ package edu.illinois.ncsa.cyberintegrator.service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.apache.http.HttpEntity;
@@ -11,13 +14,20 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.illinois.ncsa.cyberintegrator.domain.Execution.State;
+import edu.illinois.ncsa.cyberintegrator.domain.Submission;
 import edu.illinois.ncsa.cyberintegrator.domain.Workflow;
 import edu.illinois.ncsa.domain.FileDescriptor;
 
@@ -27,43 +37,136 @@ public class CyberintegratorServiceClient {
 
     public static String  SERVER = "";
 
-//    public static FileDescriptor post(File file) {
-//        String responseStr = null;
-//        FileDescriptor fd = null;
-//
-//        HttpClient httpclient = new DefaultHttpClient();
-//
-//        try {
-//            String requestUrl = SERVER + "/files";
-//            HttpPost httppost = new HttpPost(requestUrl);
-//
-//            FileBody bin = new FileBody(file);
-//
-//            MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
-//            reqEntity.addPart("uploadedFile", bin);
-//
-//            httppost.setEntity(reqEntity);
-//
-//            logger.info("executing request " + httppost.getRequestLine());
-//
-//            ResponseHandler<String> responseHandler = new BasicResponseHandler();
-//            try {
-//                responseStr = httpclient.execute(httppost, responseHandler);
-//                logger.debug("response string" + responseStr);
-//                ObjectMapper mapper = new ObjectMapper();
-//                fd = mapper.readValue(responseStr, FileDescriptor.class);
-//            } catch (Exception e) {
-//                logger.error("HTTP post failed", e);
-//            }
-//
-//        } finally {
-//            try {
-//                httpclient.getConnectionManager().shutdown();
-//                return fd;
-//            } catch (Exception ignore) {}
-//        }
-//        return null;
-//    }
+    public static String submit(Submission submission) {
+        String executionId = null;
+
+        HttpClient httpClient = new DefaultHttpClient();
+
+        try {
+            String requestUrl = SERVER + "/executions";
+            HttpPost httppost = new HttpPost(requestUrl);
+
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(submission);
+
+            StringEntity input = new StringEntity(json);
+            input.setContentType("application/json");
+            httppost.setEntity(input);
+
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            try {
+
+                executionId = httpClient.execute(httppost, responseHandler);
+
+                logger.info("Submitting ci workflow (submission): " + httppost.getRequestLine());
+
+                logger.info("Execution id: " + executionId);
+            } catch (Exception e) {
+                logger.error("HTTP get failed", e);
+            }
+        } catch (JsonGenerationException e1) {
+            logger.error("Can't generate Json from object", e1);
+        } catch (JsonMappingException e1) {
+            logger.error("Can't generate Json from object", e1);
+        } catch (IOException e1) {
+            logger.error("Can't generate Json from object", e1);
+        } finally {
+            try {
+                httpClient.getConnectionManager().shutdown();
+                return executionId;
+            } catch (Exception ignore) {}
+        }
+        return null;
+    }
+
+    public static Map<String, State> getState(String executionId) {
+        String responseStr = null;
+        Map<String, State> states = null;
+
+        HttpClient httpclient = new DefaultHttpClient();
+        try {
+            String requestUrl = SERVER + "/executions/" + executionId + "/state";
+            HttpGet httpGet = new HttpGet(requestUrl);
+
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+            logger.info("executing request " + httpGet.getRequestLine());
+
+            try {
+                responseStr = httpclient.execute(httpGet, responseHandler);
+                logger.debug("Response String: " + responseStr);
+                ObjectMapper mapper = new ObjectMapper();
+                states = mapper.readValue(responseStr, new TypeReference<Map<String, Object>>() {});
+            } catch (Exception e) {
+                logger.error("HTTP get failed", e);
+            }
+
+        } finally {
+            try {
+                httpclient.getConnectionManager().shutdown();
+                return states;
+            } catch (Exception ignore) {}
+        }
+        return null;
+    }
+
+    public static List<String> getListOutput(String executionId) {
+        String responseStr = null;
+        List<String> result = null;
+        HttpClient httpclient = new DefaultHttpClient();
+        try {
+            String requestUrl = SERVER + "/executions/" + executionId + "/outputs";
+            HttpPut httpPut = new HttpPut(requestUrl);
+
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+            logger.info("executing request " + httpPut.getRequestLine());
+
+            try {
+                responseStr = httpclient.execute(httpPut, responseHandler);
+                logger.debug("Response String: " + responseStr);
+                ObjectMapper mapper = new ObjectMapper();
+                result = mapper.readValue(responseStr, new TypeReference<List<String>>() {});
+            } catch (Exception e) {
+                logger.error("HTTP get failed", e);
+            }
+
+        } finally {
+            try {
+                httpclient.getConnectionManager().shutdown();
+                return result;
+            } catch (Exception ignore) {}
+        }
+        return null;
+    }
+
+    public static String getOutputUrl(String executionId, String outputId) {
+        String responseStr = null;
+
+        HttpClient httpclient = new DefaultHttpClient();
+        try {
+            String requestUrl = SERVER + "/executions/" + executionId + "/outputs/" + outputId;
+            HttpPut httpPut = new HttpPut(requestUrl);
+
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+            logger.info("executing request " + httpPut.getRequestLine());
+
+            try {
+                responseStr = httpclient.execute(httpPut, responseHandler);
+                logger.debug("Response String: " + responseStr);
+            } catch (Exception e) {
+                logger.error("HTTP get failed", e);
+            }
+
+        } finally {
+            try {
+                httpclient.getConnectionManager().shutdown();
+                return responseStr;
+            } catch (Exception ignore) {}
+        }
+        return null;
+    }
 
     public static void startExecutionById(String id) {
         String responseStr = null;
