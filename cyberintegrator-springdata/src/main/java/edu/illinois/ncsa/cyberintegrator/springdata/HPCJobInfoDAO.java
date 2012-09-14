ebd7de29b38f85ea6aca8900e04109410721a985
jonglee <jonglee1@illinois.edu>
2012-09-14 18:57:03 -0500

    added query find by execution id

--- cyberintegrator-springdata/src/main/java/edu/illinois/ncsa/cyberintegrator/springdata/HPCJobInfoDAO.java
+++ cyberintegrator-springdata/src/main/java/edu/illinois/ncsa/cyberintegrator/springdata/HPCJobInfoDAO.java
@@ -1,9 +1,11 @@
 package edu.illinois.ncsa.cyberintegrator.springdata;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import edu.illinois.ncsa.cyberintegrator.domain.HPCJobInfo;
 
 public interface HPCJobInfoDAO extends PagingAndSortingRepository<HPCJobInfo, String> {
-
+    List<HPCJobInfo> findByExecutionId(String executionId);
 }
