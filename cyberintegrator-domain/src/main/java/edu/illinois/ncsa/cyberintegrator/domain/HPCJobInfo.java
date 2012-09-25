6fa4b25248c207c1d4df0003b06502dc3bc779ca
Chris Navarro <cmnavarr@illinois.edu>
2012-09-25 11:15:55 -0500

    added standard out/err to hpc info bean

--- cyberintegrator-domain/src/main/java/edu/illinois/ncsa/cyberintegrator/domain/HPCJobInfo.java
+++ cyberintegrator-domain/src/main/java/edu/illinois/ncsa/cyberintegrator/domain/HPCJobInfo.java
@@ -14,10 +14,16 @@ public class HPCJobInfo extends AbstractBean {
     private static final long serialVersionUID = 1L;
 
     /** Execution id returned from the workflows execution engine **/
-    public String             executionId      = null;
+    private String            executionId      = null;
 
     /** Directory where job is generating output */
-    public String             workingDir       = null;
+    private String            workingDir       = null;
+
+    /** File with job output */
+    private String            standardOutput   = null;
+
+    /** File with job error */
+    private String            standardError    = null;
 
     public String getExecutionId() {
         return executionId;
@@ -33,6 +39,22 @@ public class HPCJobInfo extends AbstractBean {
 
     public void setWorkingDir(String workingDir) {
         this.workingDir = workingDir;
+    }
+
+    public String getStandardOutput() {
+        return standardOutput;
+    }
+
+    public void setStandardOutput(String standardOutput) {
+        this.standardOutput = standardOutput;
+    }
+
+    public String getStandardError() {
+        return standardError;
+    }
+
+    public void setStandardError(String standardError) {
+        this.standardError = standardError;
     }
 
 }
