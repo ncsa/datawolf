a4692118b3d0022219b7bb911349965ea0625499
Rob Kooper <kooper@illinois.edu>
2013-07-20 22:56:48 -0500

    remove unused cyberintegrator-gui

--- cyberintegrator-gui/src/main/java/edu/illinois/ncsa/cyberintegrator/gui/ExecutorWizard.java
+++
@@ -1,9 +0,0 @@
-package edu.illinois.ncsa.cyberintegrator.gui;
-
-import edu.illinois.ncsa.cyberintegrator.domain.WorkflowTool;
-
-public interface ExecutorWizard {
-    String getTitle();
-
-    void showWizard(WorkflowTool oldtool);
-}
