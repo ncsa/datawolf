d995ff19df249071860f7ad71901cb64bdab4c5d
Chris Navarro <cmnavarr@illinois.edu>
2013-12-19 10:28:05 -0600

    CBI-480 added registration page to login screen

--- cyberintegrator-editor/src/main/webapp/js/login.js
+++ cyberintegrator-editor/src/main/webapp/js/login.js
@@ -1,5 +1,6 @@
 var personCollection = new PersonCollection();
 var showingLoginError = false;
+var showingRegistrationError = false;
 
 var AppRouter = Backbone.Router.extend({
     routes:{
@@ -11,6 +12,8 @@ var AppRouter = Backbone.Router.extend({
     	
     	personCollection.fetch({success: function() {
     		$('#login-form').html(new LoginView().render().el);
+            $('#register-form').html(new RegistrationView().render().el);
+
     		$('#username').keypress(function() {
     			if(showingLoginError) {
     				$("#login-error").hide();
@@ -22,9 +25,39 @@ var AppRouter = Backbone.Router.extend({
     				$("#login-error").hide();
     			}
     		});
+
+            $('#firstname').keypress(registrationError);
+            $('#lastname').keypress(registrationError);
+            $('#email').keypress(registrationError);
+            $('#newpassword').keypress(registrationError);
+
     	}});
     }
 });
+
+var registrationError = function() {
+    if(showingRegistrationError) {
+        $("#registration-error").hide();
+    }
+};
+
+var createPerson = function(firstName, lastName, email) {
+    var url = '/persons?'+'firstname='+firstName+'&lastname='+lastName+'&email='+email;
+    $.ajax({
+        type: "POST",
+        url: url,
+        dataType: "text",
+
+        success: function(msg) {
+            console.log("created person with id = "+msg);
+            localStorage.currentUser = msg;
+            location.replace("index.html");
+        },
+        error: function(msg) {
+            alert('error: '+JSON.stringify(msg));
+        }
+    }); 
+}
 
 var app = new AppRouter();
 
--- cyberintegrator-editor/src/main/webapp/js/views/loginview.js
+++ cyberintegrator-editor/src/main/webapp/js/views/loginview.js
@@ -39,3 +39,47 @@ var LoginView = Backbone.View.extend({
 	}
 
 });
+
+var RegistrationView = Backbone.View.extend({
+	template: _.template($('#register-view-template').html()),
+
+	events: {
+		'click button#register-btn' : 'registerUser'
+	},
+
+	initialize: function() {
+
+	},
+
+	render: function() {
+		$(this.el).html(this.template());
+		return this;
+	},
+
+	registerUser: function(e) {
+		e.preventDefault();
+
+		var email = $('input[name=email]').val();
+
+		var user = null;
+		// Find if person exists
+		personCollection.each(function(person) {
+			if(person.get('email') === email) {
+				user = person;
+				return false;
+			}
+		});
+
+		if(user != null) {
+			showingRegistrationError = true;
+			$("#registration-error").show();
+		} else {
+			var firstName = $('input[name=firstname]').val();
+			var lastName = $('input[name=lastname]').val();
+
+			createPerson(firstName, lastName, email);
+		}
+	}
+
+
+})
