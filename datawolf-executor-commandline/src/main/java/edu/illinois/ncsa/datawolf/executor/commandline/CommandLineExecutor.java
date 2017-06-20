0d84a79ec80dc480e64c2942cc46597680e636ef
Chris Navarro <cmnavarr@illinois.edu>
2017-06-20 11:10:09 -0500

    WOLF-228 modified command line executor to write the input dataset regardless of whether it is passed to the tool

--- datawolf-executor-commandline/src/main/java/edu/illinois/ncsa/datawolf/executor/commandline/CommandLineExecutor.java
+++ datawolf-executor-commandline/src/main/java/edu/illinois/ncsa/datawolf/executor/commandline/CommandLineExecutor.java
@@ -153,18 +153,56 @@ public class CommandLineExecutor extends LocalExecutor {
                         throw (new FailedException("Could not create temp file."));
                     }
 
-                    if (option.isCommandline()) {
-                        if (option.getInputOutput() != InputOutput.OUTPUT) {
-                            String key = step.getInputs().get(option.getOptionId());
-                            Dataset ds = datasetDao.findOne(execution.getDataset(key));// option.getOptionId()));
-                            if (ds == null) {
-                                throw (new AbortException("Dataset is missing."));
+                    if (option.getInputOutput() != InputOutput.OUTPUT) {
+                        String key = step.getInputs().get(option.getOptionId());
+                        Dataset ds = datasetDao.findOne(execution.getDataset(key));// option.getOptionId()));
+                        if (ds == null) {
+                            throw (new AbortException("Dataset is missing."));
+                        }
+
+                        // Define read/write buffer
+                        byte[] buf = new byte[10240];
+                        if (ds.getFileDescriptors().size() == 1) {
+                            try (InputStream is = fileStorage.readFile(ds.getFileDescriptors().get(0)); FileOutputStream fos = new FileOutputStream(filename);) {
+                                int len = 0;
+                                while ((len = is.read(buf)) > 0) {
+                                    fos.write(buf, 0, len);
+                                }
+                            } catch (IOException e) {
+                                throw (new FailedException("Could not get input file.", e));
+                            }
+                        } else {
+
+                            // Create a folder for the datasets
+                            File inputFolder = new File(filename);
+                            if (!inputFolder.mkdirs()) {
+                                throw (new FailedException("Could not create folder for input files"));
                             }
 
-                            // Define read/write buffer
-                            byte[] buf = new byte[10240];
-                            if (ds.getFileDescriptors().size() == 1) {
-                                try (InputStream is = fileStorage.readFile(ds.getFileDescriptors().get(0)); FileOutputStream fos = new FileOutputStream(filename);) {
+                            int duplicate = 1;
+                            for (FileDescriptor fd : ds.getFileDescriptors()) {
+                                String localFileName = fd.getFilename();
+
+                                // Check if file already exists
+                                if (new File(inputFolder, localFileName).exists()) {
+                                    int fileExtensionIndex = fd.getFilename().lastIndexOf(".");
+
+                                    String name = new File(inputFolder, localFileName).getName();
+                                    String fileExtension = "";
+                                    if (fileExtensionIndex != -1) {
+                                        fileExtension = fd.getFilename().substring(fileExtensionIndex);
+                                        logger.debug("File extension is " + fileExtension);
+                                    }
+
+                                    localFileName = new File(inputFolder, name + " (" + duplicate + ")" + fileExtension).getAbsolutePath();
+                                    duplicate++;
+                                } else {
+                                    localFileName = new File(inputFolder, localFileName).getAbsolutePath();
+                                }
+
+                                logger.debug("Input file path is " + localFileName);
+
+                                try (InputStream is = fileStorage.readFile(fd); FileOutputStream fos = new FileOutputStream(localFileName);) {
                                     int len = 0;
                                     while ((len = is.read(buf)) > 0) {
                                         fos.write(buf, 0, len);
@@ -172,49 +210,11 @@ public class CommandLineExecutor extends LocalExecutor {
                                 } catch (IOException e) {
                                     throw (new FailedException("Could not get input file.", e));
                                 }
-                            } else {
-
-                                // Create a folder for the datasets
-                                File inputFolder = new File(filename);
-                                if (!inputFolder.mkdirs()) {
-                                    throw (new FailedException("Could not create folder for input files"));
-                                }
-
-                                int duplicate = 1;
-                                for (FileDescriptor fd : ds.getFileDescriptors()) {
-                                    String localFileName = fd.getFilename();
-
-                                    // Check if file already exists
-                                    if (new File(inputFolder, localFileName).exists()) {
-                                        int fileExtensionIndex = fd.getFilename().lastIndexOf(".");
-
-                                        String name = new File(inputFolder, localFileName).getName();
-                                        String fileExtension = "";
-                                        if (fileExtensionIndex != -1) {
-                                            fileExtension = fd.getFilename().substring(fileExtensionIndex);
-                                            logger.debug("File extension is " + fileExtension);
-                                        }
-
-                                        localFileName = new File(inputFolder, name + " (" + duplicate + ")" + fileExtension).getAbsolutePath();
-                                        duplicate++;
-                                    } else {
-                                        localFileName = new File(inputFolder, localFileName).getAbsolutePath();
-                                    }
-
-                                    logger.debug("Input file path is " + localFileName);
-
-                                    try (InputStream is = fileStorage.readFile(fd); FileOutputStream fos = new FileOutputStream(localFileName);) {
-                                        int len = 0;
-                                        while ((len = is.read(buf)) > 0) {
-                                            fos.write(buf, 0, len);
-                                        }
-                                    } catch (IOException e) {
-                                        throw (new FailedException("Could not get input file.", e));
-                                    }
-                                }
                             }
                         }
-                        command.add(filename);
+                        if (option.isCommandline()) {
+                            command.add(filename);
+                        }
                     }
 
                     if (option.getInputOutput() != InputOutput.INPUT) {
