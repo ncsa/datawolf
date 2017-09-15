74afbbc21b3effbcd9994229a889acf351d24300
Chris Navarro <cmnavarr@illinois.edu>
2017-09-15 13:15:10 -0500

    WOLF-225 added paging methods to DatasetDAO interface and started implementing paging for the common dataset dao methods

--- ncsa-common-jpa/src/main/java/edu/illinois/ncsa/jpa/dao/DatasetJPADao.java
+++ ncsa-common-jpa/src/main/java/edu/illinois/ncsa/jpa/dao/DatasetJPADao.java
@@ -30,9 +30,27 @@ public class DatasetJPADao extends AbstractJPADao<Dataset, String> implements Da
         return results;
     }
 
+    @Transactional
+    public List<Dataset> findByDeleted(boolean deleted, int page, int size) {
+        List<Dataset> results = null;
+        String queryString = "SELECT d FROM Dataset d " + "WHERE d.deleted = :deleted";
+        TypedQuery<Dataset> q = getEntityManager().createQuery(queryString, Dataset.class);
+        q.setParameter("deleted", deleted);
+        q.setFirstResult(page * size);
+        q.setMaxResults(size);
+        results = q.getResultList();
+        return results;
+    }
+
     @Override
     public List<Dataset> findByTitleLike(String titlePattern) {
         // TODO implement query
+        return null;
+    }
+
+    @Override
+    public List<Dataset> findByTitleLike(String titlePattern, int page, int size) {
+        // TODO Auto-generated method stub
         return null;
     }
 
@@ -43,6 +61,11 @@ public class DatasetJPADao extends AbstractJPADao<Dataset, String> implements Da
     }
 
     @Override
+    public List<Dataset> findByTitleLikeAndDeleted(String titlePattern, boolean deleted, int page, int size) {
+        // TODO implement query
+        return null;
+    }
+
     @Transactional
     public List<Dataset> findByCreatorEmail(String email) {
         List<Dataset> results = null;
@@ -58,6 +81,21 @@ public class DatasetJPADao extends AbstractJPADao<Dataset, String> implements Da
 
     @Override
     @Transactional
+    public List<Dataset> findByCreatorEmail(String email, int page, int size) {
+        List<Dataset> results = null;
+        String queryString = "SELECT d FROM Dataset d " + "WHERE d.creator.email = :email";
+
+        TypedQuery<Dataset> q = getEntityManager().createQuery(queryString, Dataset.class);
+        q.setParameter("email", email);
+        q.setFirstResult(page * size);
+        q.setMaxResults(size);
+        results = q.getResultList();
+
+        return results;
+
+    }
+
+    @Transactional
     public List<Dataset> findByCreatorEmailAndDeleted(String email, boolean deleted) {
         List<Dataset> results = null;
         String queryString = "SELECT d FROM Dataset d " + "WHERE d.creator.email = :email and d.deleted = :deleted";
@@ -70,9 +108,30 @@ public class DatasetJPADao extends AbstractJPADao<Dataset, String> implements Da
         return results;
     }
 
+    @Transactional
+    public List<Dataset> findByCreatorEmailAndDeleted(String email, boolean deleted, int page, int size) {
+        List<Dataset> results = null;
+        String queryString = "SELECT d FROM Dataset d " + "WHERE d.creator.email = :email and d.deleted = :deleted";
+
+        TypedQuery<Dataset> q = getEntityManager().createQuery(queryString, Dataset.class);
+        q.setParameter("email", email);
+        q.setParameter("deleted", deleted);
+        q.setFirstResult(page * size);
+        q.setMaxResults(size);
+        results = q.getResultList();
+
+        return results;
+    }
+
     @Override
     public List<Dataset> findByCreatorEmailAndTitleLike(String email, String titlePattern) {
         // TODO implement query
+        return null;
+    }
+
+    @Override
+    public List<Dataset> findByCreatorEmailAndTitleLike(String email, String titlePattern, int page, int size) {
+        // TODO Auto-generated method stub
         return null;
     }
 
@@ -81,4 +140,11 @@ public class DatasetJPADao extends AbstractJPADao<Dataset, String> implements Da
         // TODO implement query
         return null;
     }
+
+    @Override
+    public List<Dataset> findByCreatorEmailAndTitleLikeAndDeleted(String email, String titlePattern, boolean deleted, int page, int size) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
