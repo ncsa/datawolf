43a4f5139b0cb1516652b578c200ef42a8dfd11c
Chris Navarro <cmnavarr@illinois.edu>
2020-10-16 08:37:39 -0500

    Updated release script to tag and push docker images

--- release.sh
+++ release.sh
@@ -6,20 +6,39 @@ set -e
 # use DEBUG=echo ./release-all.sh to print all commands
 export DEBUG=${DEBUG:-""}
 
-$DEBUG docker login hub.ncsa.illinois.edu
-
 # Find out what branch we are on
 BRANCH=${BRANCH:-"$(git rev-parse --abbrev-ref HEAD)"}
 
-# Find out the version
-if [ "$BRANCH" = "master" ]; then
-    VERSION=""
+# use SERVER=XYZ/ to push to a different server
+SERVER=${SERVER:-""}
+
+# find out the version
+if [ "${BRANCH}" = "master" ]; then
+    VERSION=${VERSION:-""}
+    if [ "${VERSION}" = "" ]; then
+      TMPVERSION=$(cat pom.xml | grep "<version>.*</version>" | head -1 |awk -F'[><]' '{print $3}')
+        echo "Detected version ${TMPVERSION}"
+        VERSION="latest"
+        OLDVERSION=""
+        while [ "$OLDVERSION" != "$TMPVERSION" ]; do
+            VERSION="${VERSION} ${TMPVERSION}"
+            OLDVERSION="${TMPVERSION}"
+            TMPVERSION=$(echo ${OLDVERSION} | sed 's/\.[0-9]*$//')
+        done
+    fi
 elif [ "${BRANCH}" = "develop" ]; then
-    VERSION="-dev"
+    VERSION="develop"
 else
     exit 0
 fi
 
-#TODO this should be replaced with tagging
-# Push all images
-docker push hub.ncsa.illinois.edu/datawolf/datawolf:latest$VERSION
+# tag and push all images
+for i in datawolf; do
+    for v in ${VERSION}; do
+	if [ "$v" != "latest" -o "$SERVER" != "" ]; then
+            ${DEBUG} docker tag datawolf/${i}:latest ${SERVER}datawolf/${i}:${v}
+        fi
+	${DEBUG} docker push ${SERVER}datawolf/${i}:${v}
+    done
+done
+    
